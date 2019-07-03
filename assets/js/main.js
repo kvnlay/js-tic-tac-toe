087746c2d011229a4f4fb04c93278d269f54662c
kvnlay <kingstevens007@gmail.com>
2019-07-03 16:22:09 +0100

    Add game module and refactor player factory function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const gameBoard = (() => {
   const matrix = new Array(9).fill(0);
   const getMatrix = () => matrix;
   const setCell = (cell, symbol) => {
+
     matrix[cell] = symbol;
   }
   const getCell = (cell) => matrix[cell];
@@ -19,16 +20,18 @@ const gameBoard = (() => {
 })()
 
 const game = (() => {
+  const currentPlayer = undefined;
+  const turns = 0;
   const winningCombinations = [
     [0,1,2], [3,4,5], [6,7,8], [0,3,6],
     [0,4,8], [1,4,7], [2,4,6], [2,5,8]
   ];
 
-  const gameWon = (symbol) => {
+  const gameWon = () => {
     for(comb of winningCombinations){
       let i = 0;
       let arr = gameBoard.getMatrix();
-      if(arr[comb[i]] === symbol){
+      if(arr[comb[i]] != 0){
         if(arr[comb[i]] == arr[comb[i + 1]] && arr[comb[i]] == arr[comb[i + 2]]){
           return true;
         }
@@ -38,15 +41,32 @@ const game = (() => {
   }
 
   const isTie = () => gameBoard.isFull && !gameWon;
+
   const gameOver = () => gameWon || isTie;
+
+  const getCurrentPlayer = () => (turns % 2 === 0)? currentPlayer = player1 : player2;
+  
+  const increaseTurn = () => turn++;
+  return {
+    gameWon,
+    isTie,
+    gameOver,
+    getCurrentPlayer,
+    increaseTurn
+  }
 })()
 
 const displayController = (() => {
+  const render = () => {
+
+  }
+
 
 })()
 
-const players = (name, symbol) => {
-  const getName = () => name;
-  const getSymbol = () => symbol;
-  return {getName, getSymbol};
+const Player = (name, symbol) => {
+  return {name, symbol};
 }
+
+let player1 = Player('Player 1', 'X');
+let player2 = Player('Player 2', 'O');
