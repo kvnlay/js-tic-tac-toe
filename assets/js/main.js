fa852345779013738710b0ab7088b553243d9bf3
kvnlay <kingstevens007@gmail.com>
2019-07-04 13:32:49 +0100

    refactor display controller module

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const Player = (name, symbol) => {
-  return {name, symbol};
+  const setPlayerName = (name) => name = name;
+  return {name, symbol, setPlayerName};
 }
 
 let player1 = Player('Player 1',  'X');
@@ -46,9 +47,11 @@ const game = (() => {
     return false;
   }
 
-  const isTie = () => gameBoard.isFull && !gameWon;
+  const isTie = () => {
+    return !gameBoard.getMatrix().some(pos => pos === 0);
+  }
 
-  const gameOver = () => gameWon || isTie;
+  const gameOver = () => gameWon() || isTie();
 
   const getCurrentPlayer = () => {
     if(turns % 2 === 0){
@@ -77,34 +80,64 @@ const game = (() => {
 
 const displayController = (() => {
   const cells = document.querySelectorAll('.cell');
+  let status = document.querySelector('.status');
+
   const render = () => {
-    for(let cell of cells){
-      cell.innerHTML = '';
-      cell.addEventListener("click", (e) => {
-        _markCell(e);
-        game.setCurrentPlayer();
-        e.target.classList.add('disabled');
-      })
+    const resetGame = () => {
+      for(let cell of cells){
+        cell.innerHTML = '';
+        cell.classList.remove('disabled', 'o-color', 'x-color');
+        cell.addEventListener("click", _playgame, false)
+      }
     }
 
     document.getElementById("reset_btn").addEventListener("click", () => {
       game.resetTurns();
       gameBoard.reset();
-      for(let cell of cells){
-        cell.classList.remove('disabled');
-        cell.innerHTML = '';
-      }
+      resetGame();
+      status.classList.add('hidden');
+      status.innerHTML = "";
     });
-  }
-  
 
-  
+    resetGame();
+  }
+  const _playgame = (e) => {
+    _markCell(e);
+    e.target.classList.add('disabled');
+    _showStatus();
+  }
+
   const _markCell = (cell) => {
     let currentPlayer = game.getCurrentPlayer();
     let box = document.getElementById(cell.target.id);
     if(gameBoard.isAvailable(cell.target.id)){
       gameBoard.setCell(cell.target.id, currentPlayer.symbol)
+      box.classList.add(game.getCurrentPlayer() === player1 ? "x-color" : "o-color");
       box.innerHTML = currentPlayer.symbol;
+    }
+  }
+
+  const _showStatus = () => {
+    let currentPlayer = game.getCurrentPlayer();
+    if(game.gameWon()){
+      status.classList.remove('hidden');
+      status.innerHTML = `${currentPlayer.name} has won the game`;
+      _gameOver();
+    }else if(game.isTie()){
+      _gameOver();
+      status.classList.remove('hidden');
+      status.innerHTML = "The game is a tie"
+    }else{
+      game.setCurrentPlayer();
+    }
+  }
+
+  const _gameOver = () => {
+    if(game.gameOver()){
+      for(let i = 0; i < cells.length; i++){
+        cells[i].removeEventListener('click', _playgame, false);
+        cells[i].classList.add('disabled');
+      }
     }
   }
 
